5a6fcd003a510a9c4da0dde485affc5959f009da
shes094101 <32055777+shes094101@users.noreply.github.com>
2017-12-18 01:46:15 +0800

    123

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;//撲克牌大小
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner src =new Scanner(System.in);
+        int y =src.nextInt();
+        String z =src.next();
+ //           int [][] x =new int[][];
+            for (int i=0 ; i<y ;i++){
+  //              for (int j=0 ; j<x.length ;j++){
+
+                }
+                System.out.println();
+            }
+    }
+//}
